4ce68df4796c1eeec940a17fc3387029175309a9
Brian <brian.1980.habeeb@gmail.com>
2022-01-31 12:43:49 -0500

    security classes added

--- March-Madness-Predictor/src/main/java/com/tournament/marchmadnesspredictor/security/MyUserDetailsService.java
+++ March-Madness-Predictor/src/main/java/com/tournament/marchmadnesspredictor/security/MyUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.tournament.marchmadnesspredictor.security;
 
+import com.food.recipes.model.User;
+import com.food.recipes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+
+
+
+}
 public class MyUserDetailsService {
 }
